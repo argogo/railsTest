228632336b20a73e9da3e6264c5290360ed133ec
arpcso <a.gomez18384@gmail.com>
2017-01-31 18:23:30 -0800

    4.3 Create controller & views for orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
+
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
-	root 'static_pages#index'
+	root 'static_pages#landing_page'
   get 'static_pages/index'
 
   get 'static_page/index'
