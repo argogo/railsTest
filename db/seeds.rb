4fe294e5e5a2f4f0b94a8e0966302a5d0df7051f
arpcso <a.gomez18384@gmail.com>
2017-02-14 21:46:26 -0800

    5.1 Update site root and create redirect

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,6 @@
 
 Product.create(:name => "Jeans1", :description => "Seed generated blue jeans. Womens fit", :colour => "Denim Blue", :image_url => "https://static.pexels.com/photos/42466/pexels-photo-42466.jpeg", :price => 11.99)
 Product.create(:name => "Jeans2", :description => "Seed generated blue jeans. Mens fit.", :colour => "Denim Blue", :image_url => "https://upload.wikimedia.org/wikipedia/commons/d/d2/Jeans_for_men.jpg", :price => 23.67)
-Product.create(:name => "Shirt1", :description => "Seed generated D.A.R.E. shirt. Unisex fit.", :colour => "White", :image_url => "https://upload.wikimedia.org/wikipedia/commons/3/38/DAREt-shirt_IGP4794.jpg", :price => 10.21)
+Product.create(:name => "Shirt1", :description => "Seed generated T-Shirt. Unisex fit.", :colour => "Salmon", :image_url => "https://upload.wikimedia.org/wikipedia/commons/5/56/T-Shirt_Orange_Fuse_-_Orus_Clothing.png", :price => 10.21)
 Product.create(:name => "Jacket1", :description => "Seed generated jacket. Unisex fit.", :colour => "Grey", :image_url => "https://c1.staticflickr.com/9/8049/8098523438_e0d15cd826.jpg", :price => 33.41)
 
