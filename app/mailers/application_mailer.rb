618d95b5f6ae6078ce6fae0c39c4e8507bb8a620
arpcso <a.gomez18384@gmail.com>
2017-02-23 21:16:11 -0800

    5.3.0 Add contact form and add method for thank_you mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  def mailer(user)
+    @user = user
+    mail(to: @user.email, subject: 'Test')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
 
   get 'static_pages/landing_page'
 
+  get 'static_pages/thank_you'
+
+  post 'static_pages/thank_you'
+
   root 'static_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
