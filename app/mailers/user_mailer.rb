d490cd9bf49c4fca4e7ac304e09907a472661630
arpcso <a.gomez18384@gmail.com>
2017-02-24 20:34:30 -0800

    5.3.5 heroku debug

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,8 +3,8 @@ class UserMailer < ApplicationMailer
 
   def contact_form(email, name, message)
     @message = message
-      mail(:from => email,
-          :to => 'anilesantonio@gmail.com'
+      mail(:from => email, 
+          :to => 'anilesantonio@gmail.com',
           :subject => "A new contact form message from #{name}")
     end
   end
