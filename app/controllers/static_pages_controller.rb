5ee2354771ded6ea014ad83e5ced3aae07093e67
arpcso <a.gomez18384@gmail.com>
2017-02-24 20:09:04 -0800

    5.3.5 heorku debug

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -15,14 +15,6 @@ class StaticPagesController < ApplicationController
 
 
   
-  def thank_you
-    @name = params[:name]
-    @email = params[:email] 
-    @message = params[:message]
-    ActionMailer::Base.mail(:from => @email,
-      :to => 'anilesantonio@gmail.com',
-      :subject => "A new contact form message from #{@name}",
-      :body => @message).deliver_now
-  end
+
 
 end
