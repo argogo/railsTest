769e54e1cff8de2d7180f04fead0ef2883dd8518
arpcso <a.gomez18384@gmail.com>
2017-02-24 18:48:00 -0800

    5.3.2 Config controller mailer SUCCESSFUL

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -7,14 +7,6 @@ class ApplicationMailer < ActionMailer::Base
     mail(to: @user.email, subject: 'Test')
   end
 
-  def thank_you
-    @name = params[:name]
-    @email = params[:email] 
-    @message = params[:message]
-    ActionMailer::Base.mail(:from => @email,
-      :to => 'anilesantonio@gmail.com',
-      :subject => "A new contact form message from #{@name}",
-      :body => @message).deliver_now
-  end
+
 
 end
