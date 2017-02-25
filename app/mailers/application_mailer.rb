7ec776ed12b27e0fda0e391724ca6fcf3072db81
arpcso <a.gomez18384@gmail.com>
2017-02-24 20:05:54 -0800

    5.3.5 heroku debug

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,10 +2,6 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'a.gomez18384@gmail.com'
   layout 'mailer'
 
-  def mailer(user)
-    @user = user
-    mail(to: @user.email, subject: 'Test')
-  end
 
 
 
