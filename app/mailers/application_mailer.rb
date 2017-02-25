6e9f49e7c24244ad07eaebef6b804e631c0cdc41
arpcso <a.gomez18384@gmail.com>
2017-02-24 18:29:32 -0800

    5.3.1 Config smtp in settings/envi.rb

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -6,4 +6,15 @@ class ApplicationMailer < ActionMailer::Base
     @user = user
     mail(to: @user.email, subject: 'Test')
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email] 
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+      :to => 'anilesantonio@gmail.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+  end
+
 end
