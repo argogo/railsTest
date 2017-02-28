1c86a8f6f9aeca7b9ef4d6d109d43a74cd48e39d
arpcso <a.gomez18384@gmail.com>
2017-02-27 21:03:08 -0800

    5.4.0 Created login forms with functionality using devise gem

--- Gemfile
+++ Gemfile
@@ -35,6 +35,9 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'devise'
+# User login and authentication
+
 group :production do
   gem 'pg'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  resources :users
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
 
@@ -9,8 +11,6 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   get 'static_pages/landing_page'
-
-
 
   post 'static_pages/thank_you'
 
