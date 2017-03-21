3c9a8844b287994c68fe20f593bd31cd91c5952c
arpcso <a.gomez18384@gmail.com>
2017-03-20 21:13:29 -0700

    5.11 Change colors in scss sheets

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
 
   post 'static_pages/thank_you'
 
-  root :to => 'static_pages#index'
+  root :to => 'static_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
