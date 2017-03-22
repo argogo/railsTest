cc7e16cfe968fb4beb08ca0d77661e77f5bb0e55
arpcso <a.gomez18384@gmail.com>
2017-03-21 17:59:45 -0700

    5.11.1 Only admin has permissions to delete products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!, :except => [:show, :index]
+  load_and_authorize_resource
 
   # GET /products
   # GET /products.json
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,9 @@ class Ability
     user ||= User.new #guest user (not logged in)
     if user.admin?
       can :manage, :all
+      can :destroy, Product
     else
+      cannot :destroy, Product
       can :read, :all
       can :manage, User, id: user.id
     end
