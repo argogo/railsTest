bfad03492e52e484fe15d4fffbbbeee06f3df893
arpcso <a.gomez18384@gmail.com>
2017-03-25 16:47:53 -0700

    changed name: Cool Shirt

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 describe Product do
   before do
-    @product = Product.create!(name: "race bike");
+    @product = Product.create!(name: "Cool Shirt");
+    @user = User.create!(email: "johns@example.com", password: "password1");
+    @product.comments.create! (rating: 1, user: @user, body: "a bad bike");
+    @product.comments.create! (rating: 5, user: @user, body: "an outstanding bike"); 
+    @product.comments.create!  (rating: 3, user: @user, body: "an extremely average bike");
+  end
+
+  it "returns the average ratings of all comments" do
+    expect ()
   end
 end
