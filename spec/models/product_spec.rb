9e6244710be397c6abbfeacca0904facff30e2b0
arpcso <a.gomez18384@gmail.com>
2017-03-26 17:42:20 -0700

    Added an average variable to compare test with

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,12 +4,17 @@ describe Product do
   before do
     @product = Product.create! name: "Cool Shirt"
     @user = User.create! email: "johns@example.com", password: "password1"
-    @product.comments.create! rating: 1, user: @user, body: "a bad bike"
-    @product.comments.create! rating: 5, user: @user, body: "an outstanding bike"
-    @product.comments.create!  rating: 3, user: @user, body: "an extremely average bike"
+    @product_input = {
+      @product.comments.create! rating: 1, user: @user, body: "a bad bike",
+      @product.comments.create! rating: 5, user: @user, body: "an outstanding bike",
+      @product.comments.create!  rating: 3, user: @user, body: "an extremely average bike"
+    }
+    @average = (@product_input.each { 
+      |input| sum+=input.rating
+    }).to_f /@product_input.size
   end
 
   it "should expect ratings average to equal 3" do
-    expect (ratings.average).to eq 3
+    expect (@average).to eq 3
   end
 end
