df99745a50d9dd636a0d457e5e0d5e7a11672f6e
arpcso <a.gomez18384@gmail.com>
2017-03-26 19:17:28 -0700

    finished average variable, test fails properly

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,17 +2,16 @@ require 'rails_helper'
 
 describe Product do
   before do
-    @product = Product.create! name: "Cool Shirt"
-    @user = User.create! email: "johns@example.com", password: "password1"
-    @product_input = {
-      @product.comments.create! rating: 1, user: @user, body: "a bad bike",
-      @product.comments.create! rating: 5, user: @user, body: "an outstanding bike",
-      @product.comments.create!  rating: 3, user: @user, body: "an extremely average bike"
-    }
-    @average = (@product_input.each { 
-      |input| sum+=input.rating
-    }).to_f /@product_input.size
+    @product = Product.create!(name: "Cool Shirt")
+    @user = User.create!(email: "johns@example.com", password: "password1")
+    product = @product.comments.create!(rating: 1, user: @user, body: "a bad bike")
+    product = @product.comments.create!(rating: 5, user: @user, body: "an outstanding bike")
+    product = @product.comments.create!(rating: 3, user: @user, body: "an extremely average bike")
+    @average = (((product.comment.rating.each do |integer| sum+=integer end).to_f) /product.size)
+
   end
+
+
 
   it "should expect ratings average to equal 3" do
     expect (@average).to eq 3
